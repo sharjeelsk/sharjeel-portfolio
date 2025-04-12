a95d3c3a734cb2a07c8ad7bb988fb766f48aa2f8
Sharjeel's Private <Sharjeel@Sharjeels-MacBook.local>
2025-04-12 11:52:06 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Hero from './sections/hero/Hero'
+import "./styles/main.scss"
+
+
+export default function App() {
+  return (
+    <Hero />
+  )
+}
---
+++ src/sections/hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './Hero.module.scss';
+
+export default function Hero() {
+  return (
+    <section className={styles.hero}>
+      <div className={styles.info}>
+        <p>"Vision without execution is just hallucination."</p>
+        <h2>
+          Specializing in the <span>MERN</span> stack, I transform business ideas
+          into powerful, scalable, and user-friendly applications.
+        </h2>
+        <p>Here's</p>
+        <h1 className={styles.gradientText}>~ Sharjeel Shaikh</h1>
+      </div>
+
+      <div className={styles.profileSection}>profile section</div>
+    </section>
+  );
+}
